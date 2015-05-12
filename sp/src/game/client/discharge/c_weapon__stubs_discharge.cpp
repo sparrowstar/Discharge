9a47993c24de3745e3b4c3d4463c37dd8287c621
The-Yiffy-Fox <8000kevin@gmail.com>
2015-05-11 21:23:09 -0400

    Fixed weapon/compile errors

---
+++ sp/src/game/client/discharge/c_weapon__stubs_discharge.cpp
@@ -0,0 +1,19 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//=============================================================================//
+
+#include "cbase.h"
+#include "c_weapon__stubs.h"
+#include "basehlcombatweapon_shared.h"
+#include "c_basehlcombatweapon.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+
+STUB_WEAPON_CLASS(weapon_wrench, WeaponWrench, C_BaseHLBludgeonWeapon);
+STUB_WEAPON_CLASS(weapon_ak47, WeaponAK47, C_HLMachineGun);
+
+
